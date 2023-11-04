ddf5813052bc49d4ed0c21a7b2e433eb2b7e0d9a
TudorPCT <tcosmin.pasat@gmail.com>
2023-11-04 19:27:49 +0200

    Server setup

---
+++ DIP/src/test/java/dip/api/dip/DipApplicationTests.java
@@ -0,0 +1,13 @@
+package dip.api.dip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
