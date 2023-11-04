0eaadece8c00f9ce955b5e614761d49a78adb9bd
TudorPCT <tcosmin.pasat@gmail.com>
2023-11-04 19:48:24 +0200

    Server Setup

---
+++ DocumentIntegrityProviderBE/src/main/java/fii/dip/api/DocumentIntegrityProviderBeApplication.java
@@ -0,0 +1,13 @@
+package fii.dip.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentIntegrityProviderBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentIntegrityProviderBeApplication.class, args);
+    }
+
+}
---
+++ DocumentIntegrityProviderBE/src/test/java/fii/dip/api/DocumentIntegrityProviderBeApplicationTests.java
@@ -0,0 +1,13 @@
+package fii.dip.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentIntegrityProviderBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
