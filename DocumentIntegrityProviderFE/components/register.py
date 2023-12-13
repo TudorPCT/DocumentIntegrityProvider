32f0e8defab28d71313b2a81ee97a7652afacabd
TudorAP <aungurencitudor@gmail.com>
2023-12-13 22:44:37 +0200

    Register page

--- DocumentIntegrityProviderFE/components/register.py
+++ DocumentIntegrityProviderFE/components/register.py
@@ -1,5 +1,9 @@
+import PySimpleGUI as sg
+
 from services.AuthService import AuthService
 
+from components.main_gui import MainGUI
+from components.register import Register
 
 class Register:
     window = None
@@ -10,7 +14,23 @@ class Register:
 
     @staticmethod
     def build_layout():
-        pass
+        layout = [
+            [sg.Text('Email:'), sg.InputText(key='email')],
+            [sg.Text('Password:'), sg.InputText(key='password')],
+            [sg.Button('Register')]
+        ]
+        return layout
 
     def run(self):
-        pass
+        while True:
+            event, values = self.window.read()
+            if event == sg.WIN_CLOSED:
+                break
+            elif event == 'Register':
+                main_gui = MainGUI()
+                main_gui.window.Layout = Register.build_layout()
+                main_gui.current_component = Register(main_gui.window)
+                main_gui.run()
+                return
+
+        self.window.close()
