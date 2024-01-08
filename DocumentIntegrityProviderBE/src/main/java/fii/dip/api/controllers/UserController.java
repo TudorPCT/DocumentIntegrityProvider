8599ed102b9f571e18b51112ab2877d618a9cbee
Puscasu Bogdan <bogdan_puscasu2001@yahoo.com>
2024-01-08 20:00:30 +0200

    get user email from id

---
+++ DocumentIntegrityProviderBE/src/main/java/fii/dip/api/controllers/UserController.java
@@ -0,0 +1,25 @@
+package fii.dip.api.controllers;
+
+import fii.dip.api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{userId}/email")
+    public ResponseEntity<String> getUserEmailById(@PathVariable String userId) {
+        return userRepository.findEmailById(userId)
+                .map(email -> ResponseEntity.ok().body(email))
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
