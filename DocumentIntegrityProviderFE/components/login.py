35b53edbeea068d3073f7a800e4a24f81f4cd806
TudorAP <aungurencitudor@gmail.com>
2024-01-10 01:11:57 +0200

    Regex verification for email and password

--- DocumentIntegrityProviderFE/components/login.py
+++ DocumentIntegrityProviderFE/components/login.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import re
 
 from components.main_gui import MainGUI
 from services.AuthService import AuthService
@@ -27,12 +28,47 @@ class Login:
         ]
         return layout
 
+    @staticmethod
+    def validate_email(email):
+        return re.search(r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$', email)
+
+    @staticmethod
+    def validate_password(password):
+        return re.search(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$', password)
+
+    # def on_typing(self, event, value):
+    #     if event == 'email':
+    #         if not self.validate_email(value):
+    #             self.window['email'].update('Invalid email format', text_color='red')
+    #             self.window['email'].Widget.config(bg='blue')
+    #         else:
+    #             self.window['email'].update('')
+    #             self.window['email'].Widget.config(bg='white')
+    #     elif event == 'password':
+    #         if not self.validate_password(value):
+    #             self.window['password'].update('Password must be at least 8 characters long, contain at least '
+    #                                                  'one uppercase letter, one lowercase letter, one digit and one '
+    #                                                  'special character', text_color='red')
+    #             self.window['password'].Widget.config(bg='blue')
+    #         else:
+    #             self.window['password'].update('')
+    #             self.window['password'].Widget.config(bg='white')
+
     def run(self):
         while True:
             event, values = self.window.read()
             if event == sg.WIN_CLOSED:
                 break
             elif event == 'Login':
+                if not self.validate_email(values['email']):
+                    sg.popup('Invalid email format!', title='Error')
+                    continue
+                elif not self.validate_password(values['password']):
+                    sg.popup('Invalid password format!\n' 
+                             'Password must be at least 8 characters long, contain at least '
+                             'one uppercase letter, one lowercase letter, one digit and one '
+                             'special character', title='Error')
+                    continue
                 from components.document_signer_gui import DocumentSignerGUI
                 self.auth_service.login(values['email'], values['password'])
                 sg.popup('Login successful!', title='Success')
@@ -40,6 +76,15 @@ class Login:
                 app = DocumentSignerGUI()
                 app.run()
             elif event == 'Register':
+                if not self.validate_email(values['email']):
+                    sg.popup('Invalid email format!', title='Error')
+                    continue
+                elif not self.validate_password(values['password']):
+                    sg.popup('Invalid password format!\n' 
+                             'Password must be at least 8 characters long, contain at least '
+                             'one uppercase letter, one lowercase letter, one digit and one '
+                             'special character', title='Error')
+                    continue
                 from components.register import Register
                 main_gui = MainGUI()
                 self.window['title'].update(value='Register')
@@ -49,5 +94,7 @@ class Login:
                 main_gui.run()
                 self.window.close()
                 return
+            else:
+                self.on_typing(event, values[event])
 
         self.window.close()
--- DocumentIntegrityProviderFE/components/register.py
+++ DocumentIntegrityProviderFE/components/register.py
@@ -1,4 +1,6 @@
 import PySimpleGUI as sg
+import re
+
 
 from components.document_signer_gui import DocumentSignerGUI
 from services.AuthService import AuthService
@@ -15,12 +17,29 @@ class Register:
         self.auth_service = AuthService()
         self.window = window
 
+    @staticmethod
+    def validate_email(email):
+        return re.search(r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$', email)
+
+    @staticmethod
+    def validate_password(password):
+        return re.search(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$', password)
+
     def run(self):
         while True:
             event, values = self.window.read()
             if event == sg.WIN_CLOSED:
                 break
             elif event == 'Register':
+                if not self.validate_email(values['email']):
+                    sg.popup('Invalid email format!', title='Error')
+                    continue
+                elif not self.validate_password(values['password']):
+                    sg.popup('Invalid password format!\n' 
+                             'Password must be at least 8 characters long, contain at least '
+                             'one uppercase letter, one lowercase letter, one digit and one '
+                             'special character', title='Error')
+                    continue
                 if values['password'] != values['confirm_password']:
                     sg.popup('Passwords do not match!', title='Error')
                 else:
@@ -30,6 +49,15 @@ class Register:
                     app = DocumentSignerGUI()
                     app.run()
             elif event == 'Login':
+                if not self.validate_email(values['email']):
+                    sg.popup('Invalid email format!', title='Error')
+                    continue
+                elif not self.validate_password(values['password']):
+                    sg.popup('Invalid password format!\n' 
+                             'Password must be at least 8 characters long, contain at least '
+                             'one uppercase letter, one lowercase letter, one digit and one '
+                             'special character', title='Error')
+                    continue
                 from components.login import Login
                 main_gui = MainGUI()
                 self.window['title'].update(value='Login')
